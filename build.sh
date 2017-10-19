f3ab2a39ac84bdb55a83690a7f726eb457c3133c
Christoph Auer <auer.christoph@outlook.at>
2017-10-19 11:20:48 +0200

    Only add build arg if it exists

--- build.sh
+++ build.sh
@@ -2,4 +2,9 @@ if [ ! -d tdesktop ]; then
     git clone -b dev https://github.com/telegramdesktop/tdesktop.git
 fi
 
-docker build . -t tdesktop --build-arg STAGE=$1
+if [ $# -eq 0 ]
+  then
+    docker build . -t tdesktop
+else
+    docker build . -t tdesktop --build-arg STAGE=$1
+fi
